f2c3531f47c0e8c02498754870c2a5647be67158
Irfan Ainuddin <irfandin@gmail.com>
2020-06-12 02:47:22 -0700

    images loading

--- app.R
+++ app.R
@@ -1,26 +1,39 @@
 # app.R ##
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
 
 ## HEADER
 header <- dashboardHeader(title="Lassen Soils")
 
 
 ## SIDEBAR
-sidebar <- dashboardSidebar(sidebarMenu(
+sidebar <- dashboardSidebar(sidebarMenu( id = "sidebar",
   menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-  menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  selectInput("boxId",
+              label = "Choose Soil Box ID",
+              choices = df$box_id)
 ))
 
 
 ## BODY
 body <- dashboardBody( 
-  img(src=df$box_img[1], width = "200"),
-  
-  ## Tabbed items
   tabItems(
   # First tab content
-  tabItem(tabName = "dashboard"),
+  tabItem(tabName = "dashboard",
+          fluidRow(
+            box(uiOutput("boxImg")),
+            box(uiOutput("pitImg")),
+
+          ),
+          fluidRow(
+            box(uiOutput("west"), width=3),
+            box(uiOutput("north"), width=3),
+            box(uiOutput("east"), width=3),
+            box(uiOutput("south"), width=3),
+            
+          )
+  ),
   
   # Second tab content
   tabItem(tabName = "widgets")
@@ -31,10 +44,70 @@ body <- dashboardBody(
 ## BUILD UI
 ui <- dashboardPage(header,sidebar,body)
 
-
 ## BUILD SERVER FUNCTION
 server <- function(input, output, session) {
+  data <- reactive({input$boxId})
   
+  output$boxImg <- renderUI({
+    x <- input$boxId
+    x <- df %>% filter(box_id == x)
+    
+    tagList(
+      tags$img(src = x$box_img, height ="200"),
+    )
+
+
+  })
+  
+  output$pitImg <- renderUI({
+    x <- input$boxId
+    x <- df %>% filter(box_id == x)
+    
+    tagList(
+      tags$img(src = x$pit_img, height ="200")
+    )
+    
+  })
+  
+  output$west <- renderUI({
+    x <- input$boxId
+    x <- df %>% filter(box_id == x)
+    
+    tagList(
+      tags$img(src = x$west_img, width ="200")
+    )
+    
+  })
+  
+  output$north <- renderUI({
+    x <- input$boxId
+    x <- df %>% filter(box_id == x)
+    
+    tagList(
+      tags$img(src = x$north_img, width ="200")
+    )
+    
+  })
+  
+  output$east <- renderUI({
+    x <- input$boxId
+    x <- df %>% filter(box_id == x)
+    
+    tagList(
+      tags$img(src = x$east_img, width ="200")
+    )
+    
+  })
+  
+  output$south <- renderUI({
+    x <- input$boxId
+    x <- df %>% filter(box_id == x)
+    
+    tagList(
+      tags$img(src = x$south_img, width ="200")
+    )
+    
+  })
 }
 
 # Run the application 
